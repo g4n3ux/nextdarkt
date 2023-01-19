e72276100c1e7ad3c9d2cd664b6fb7e7635314f0
g4n3ux <g4n3ux@gmail.com>
2023-01-19 16:12:54 -0500

    darkmode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+
+    "./src/app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
+    "./src//pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
